786f22bb1b3d026658ce682b3bc5b7f9f1fab795
vsoch <vsoch@users.noreply.github.com>
2021-11-13 13:33:16 -0700

    try again
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- scripts/prepare-splice.py
+++ scripts/prepare-splice.py
@@ -20,12 +20,6 @@ def main(pkg):
         if tag not in content:
             sys.exit("%s not found in %s." % (tag, filename))
 
-    # Envars are for this job
-    print('echo "package=%s" >> $GITHUB_ENV' % content["package"])
-    print('echo "splice=%s" >> $GITHUB_ENV' % content["splice"])
-    print('echo "command=%s" >> $GITHUB_ENV' % content["command"])
-
-    # Outputs are for the next
     print("::set-output name=package::%s\n" % content['package'])
     print("::set-output name=splice::%s\n" % content['splice'])
     print("::set-output name=command::%s\n" % content['command'])
