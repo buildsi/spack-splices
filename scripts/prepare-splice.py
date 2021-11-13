21572be1ec8e2d553705d2044123d0200aec8ca8
vsoch <vsoch@users.noreply.github.com>
2021-11-13 14:30:00 -0700

    try again
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- scripts/prepare-splice.py
+++ scripts/prepare-splice.py
@@ -21,9 +21,9 @@ def main(pkg):
         if tag not in content:
             sys.exit("%s not found in %s." % (tag, filename))
 
-    print("::set-output name=package::%s\n" % json.dumps([content['package']]))
-    print("::set-output name=splice::%s\n" % json.dumps([content['splice']]))
-    print("::set-output name=command::%s\n" % json.dumps([content['command']]))
+    print("::set-output name=package::%s\n" % content['package'])
+    print("::set-output name=splice::%s\n" % content['splice'])
+    print("::set-output name=command::%s\n" % content['command'])
 
 
 if __name__ == "__main__":
